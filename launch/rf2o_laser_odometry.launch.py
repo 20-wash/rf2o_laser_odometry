34a64cdf3c46777b1b19f65a4e6787c4524e60be
20-wash <077bme014.biswash@pcampus.edu.np>
2025-03-14 16:42:14 +0545

    Updated rf2o_laser_odometry.launch.py

--- launch/rf2o_laser_odometry.launch.py
+++ launch/rf2o_laser_odometry.launch.py
@@ -23,11 +23,13 @@ def generate_launch_description():
                 output='screen',
                 parameters=[{
                     'laser_scan_topic' : '/scan',
-                    'odom_topic' : '/odom_rf2o',
+#                    'odom_topic' : '/odom_rf2o', 
+                    'odom_topic' : '/odom', # Changed by Biswash to match with slam toolbox
                     'publish_tf' : True,
                     'base_frame_id' : 'base_link',
                     'odom_frame_id' : 'odom',
                     'init_pose_from_topic' : '',
-                    'freq' : 20.0}],
+                    'freq' : 10.0         # Changed by Biswash 
+                    }],
             ),
     ])
